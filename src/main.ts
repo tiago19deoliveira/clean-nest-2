26f2f26ef686b0f13d0f732f40ed38f6dbbb8a9f
tiago19deoliveira <tiago18deoliveira@gmail.com>
2026-02-04 13:05:17 -0400

    feat: config service implemetantion

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module.js";
+import { ConfigService } from "@nestjs/config";
+import { performanceTimestampProvider } from "rxjs/internal/scheduler/performanceTimestampProvider";
+import { Env } from "./env.js";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService: ConfigService<Env, true> = app.get(ConfigService);
+  const port = configService.get("PORT", { infer: true });
+  await app.listen(port ?? 3000);
 }
 bootstrap();
