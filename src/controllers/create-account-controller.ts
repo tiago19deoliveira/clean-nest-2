b63f5eea7ca82dcbbdcafa03409dcb469f41e5df
tiago19deoliveira <tiago18deoliveira@gmail.com>
2026-02-04 12:43:58 -0400

    feat: zod validation and lcient request

--- src/controllers/create-account-controller.ts
+++ src/controllers/create-account-controller.ts
@@ -7,18 +7,29 @@ import {
   Controller,
   HttpCode,
   Post,
+  UsePipes,
 } from "@nestjs/common";
 import { hash } from "bcryptjs";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
 import { PrismaService } from "src/prisma/prisma.service";
+import { email, z } from "zod";
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+});
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
 
 @Controller("/accounts")
 export class CreateAccountController {
   constructor(private prisma: PrismaService) {}
   @Post()
   @HttpCode(201)
-  async handle(@Body() body: any) {
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBodySchema) {
     const { name, email, password } = body;
-
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     const userWithSameEmail = await this.prisma.user.findUnique({
       where: {
