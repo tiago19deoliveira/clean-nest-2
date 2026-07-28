af0f0d8e029ea021b1e4f1aa1a0edc4ca9488d2e
tiago19deoliveira <tiago18deoliveira@gmail.com>
2026-07-27 22:01:59 -0400

    feat: implementando strategy de env

--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -1,21 +1,23 @@
 import { Module } from "@nestjs/common";
 import { PassportModule } from "@nestjs/passport";
 import { JwtModule } from "@nestjs/jwt";
-import { ConfigService } from "@nestjs/config";
-import { Env } from "@/infra/env";
 import { JwtStrategy } from "./jwt.strategy";
 import { APP_GUARD } from "@nestjs/core";
 import { JwtAuthGuard } from "./jwt-auth-guard";
+import { EnvService } from "../env/env.service";
+import { EnvModule } from "../env/env.module";
 
 @Module({
   imports: [
+    EnvModule,
     PassportModule,
     JwtModule.registerAsync({
-      inject: [ConfigService],
+      imports: [EnvModule],
+      inject: [EnvService],
       global: true,
-      useFactory(config: ConfigService<Env, true>) {
-        const privateKey = config.get("JWT_PRIVATE_KEY", { infer: true });
-        const publicKey = config.get("JWT_PUBLIC_KEY", { infer: true });
+      useFactory(env: EnvService) {
+        const privateKey = env.get("JWT_PRIVATE_KEY");
+        const publicKey = env.get("JWT_PUBLIC_KEY");
         return {
           signOptions: { algorithm: "RS256" },
           privateKey: Buffer.from(privateKey, "base64"),
--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,12 +1,11 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module.js";
-import { ConfigService } from "@nestjs/config";
-import { Env } from "./env.js";
+import { EnvService } from "./env/env.service.js";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService: ConfigService<Env, true> = app.get(ConfigService);
-  const port = configService.get("PORT", { infer: true });
+  const envService = app.get(EnvService);
+  const port = envService.get("PORT");
   await app.listen(port ?? 3000);
 }
 bootstrap();
